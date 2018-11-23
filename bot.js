7e6348cdf84b186f05beaf2e0e2766562ef1335c
metodayn <45229634+metodayn@users.noreply.github.com>
2018-11-23 13:44:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -54,7 +54,7 @@ client2.on('message', message => {
 
 
 client.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-if (message.content === '!_1N') {
+if (message.content === '!neto') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
@@ -68,7 +68,7 @@ if (message.content === '!_1N') {
 });
 
 client2.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-if (message.content === '!_1N') {
+if (message.content === '!neto') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
